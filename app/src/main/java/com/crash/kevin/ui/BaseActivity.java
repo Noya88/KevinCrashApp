8e3bfb192177af74629eb3541470bc8e3295e79e
Noya <lixiaodonglove7@icloud.com>
2016-05-04 10:29:41 +0800

    Initial commit

---
+++ app/src/main/java/com/crash/kevin/ui/BaseActivity.java
@@ -0,0 +1,44 @@
+package com.crash.kevin.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hades on 2016/5/4.
+ */
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
---
+++ app/src/main/java/com/crash/kevin/ui/MainActivity.java
@@ -0,0 +1,45 @@
+package com.crash.kevin.ui;
+
+import android.os.Bundle;
+
+import com.crash.kevin.R;
+
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+}
